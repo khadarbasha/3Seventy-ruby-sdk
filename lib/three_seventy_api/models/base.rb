3685844248ee8e057e1fa84b37bfe92fd3f1db16
khadarbasha <khadarbasha.s@hotmail.com>
2015-02-02 21:09:34 +0530

    Documentation added for base class.

--- lib/three_seventy_api/models/base.rb
+++ lib/three_seventy_api/models/base.rb
@@ -1,5 +1,16 @@
+# Public: Base class.
 class Base
+  # Public: 
+  #       setter/getter for id.
+  #       Represents primary key of the model.
   attr_accessor :id
+  # Public: Creates the json structure for Base object.
+  #
+  # Examples
+  #
+  #   json_
+  #
+  # Returns the json structure for Base object.
   def json_
     hash = {
       "Id" => id
