28cf756ce1fbab1395ad29c2f69c76a8fccdda45
khadarbasha <khadarbasha.s@hotmail.com>
2015-02-02 20:13:27 +0530

    Default base url modified.

--- lib/three_seventy_api/helpers/request.rb
+++ lib/three_seventy_api/helpers/request.rb
@@ -2,7 +2,7 @@ module ThreeSeventyApi
   module Helpers
     module Request
       def client
-        @client ||= RestClient::Resource::new("https://api.3seventy.com/api/v2.0/", :user => @username, :password => @password)
+        @client ||= RestClient::Resource::new("https://api.3seventy.com/api/v2.0", :user => @username, :password => @password)
       end
       def get(path)
         client[path].get :accept => :json, :content_type => :json
