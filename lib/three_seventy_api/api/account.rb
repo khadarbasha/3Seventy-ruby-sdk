53df3857f0790ba25bdb80975e1f1b3b99df691e
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-06 01:43:03 +0530

    Stub definations added for account end point.

--- lib/three_seventy_api/api/account.rb
+++ lib/three_seventy_api/api/account.rb
@@ -6,6 +6,24 @@ module ThreeSeventyApi
         end_point = "/account?inactive=#{options[:inactive]}"
         get(end_point)
       end
+      def add_account(id, payload)
+        raise ThreeSeventyApi::Errors::IdMissingError if id.nil?
+        end_point = "/account/#{id}"
+        post(end_point, id, payload)
+      end
+      def delete_account(id)
+        raise ThreeSeventyApi::Errors::IdMissingError if id.nil?
+        end_point = "/account/#{id}"
+        delete(end_point, id)
+      end
+      def edit_account(id, payload)
+      end
+      # Get a specified account
+      def account(id)
+        raise ThreeSeventyApi::Errors::IdMissingError if id.nil?
+        end_point = "/account/#{id}"
+        get(end_point)
+      end
     end
   end
 end
