d5f8240487d3045b835280c1d5c544c9bc80f464
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-06 01:43:52 +0530

    Error handling at client added.

--- lib/three_seventy_api/helpers/request.rb
+++ lib/three_seventy_api/helpers/request.rb
@@ -2,11 +2,42 @@ module ThreeSeventyApi
   module Helpers
     module Request
       def client
-        @client ||= RestClient::Resource::new("https://api.3seventy.com/api/v2.0", :user => @username, :password => @password)
+        @client ||= RestClient::Resource::new("https://api.3seventy.com/api/v2.0", :user => "khadarbasha.s@hotmail.com", :password => "GUR{tA{e;;")
       end
       def get(path)
-        client[path].get :accept => :json, :content_type => :json
+       response =  client[path].get :accept => :json, :content_type => :json
+       raise_errors(response)
       end
+      def post(path, payload)
+        response = client[path].post payload
+        raise_errors(response)
+      end
+      def delete(path)
+        response = client[path].delete
+        raise_errors(response)
+      end
+      def raise_errors(response)
+          binding.pry
+          response_code = response.code
+          response_data = response_data
+          case code.to_i
+          when 401
+            response_data = response.body
+            raise ThreeSeventyApi::Errors::UnauthorizedError.new(data), "(#{response_code}): #{response_data}"
+          when 400
+            response_data = response.body
+            raise ThreeSeventyApi::Errors::GeneralError.new(data), "(#{response_code}): #{response_data}"
+          when 403
+            response_data = response.body
+            raise ThreeSeventyApi::Errors::AccessDeniedError.new(data), "(#{response_code}): #{response_data}"
+          when 404
+            raise ThreeSeventyApi::Errors::NotFoundError,"(#{response_code}): #{response_data}"
+          when 500
+            raise ThreeSeventyApi::Errors::InformLinkedInError, "Internal Server error. (#{response_code}): #{response_data}"
+          when 502..503
+            raise ThreeSeventyApi::Errors::UnavailableError, "(#{response_code}): #{response_data}"
+          end
+        end
     end
   end
 end
