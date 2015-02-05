7ab7b68714181f158c62a566afe7cfd5222b6592
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-06 01:59:37 +0530

    AccountAttribute module included in main api file.

--- lib/three_seventy_api/api.rb
+++ lib/three_seventy_api/api.rb
@@ -1,6 +1,8 @@
 require "three_seventy_api/api/account"
+require "three_seventy_api/api/account_attribute"
 module ThreeSeventyApi
   module Api
     include Account
+    include AccountAttribute
   end
 end
