7f311628e92073694f18a2803d6c2c2dcb26f429
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-07 00:20:35 +0530

    typos corrected.

--- examples/account_attribute/all_attributes.rb
+++ examples/account_attribute/all_attributes.rb
@@ -1,21 +1,21 @@
 require 'three_seventy_api'
 require 'yaml'
-# Example to show account_attributes/all_attributes end point api call.
+# Example to show AccountAttribute/AllAttributes end point api call.
 class AllAttributes
   def run
     # Load config file
     config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
-    # Load account_id from config.
+    # Load the account_id from config.
     account_id = config['account_id']
-    # Load url from config.
+    # Load the url from config.
     url = config['url']
-    # Load username from config.
+    # Load the username from config.
     username = config['username']
-    # Load password from config.
+    # Load the password from config.
     password = config['password']
     # Initiate the client.
     client = ThreeSeventyApi::Client.new(:url => url, :username => username, :password => password)
-    # Make a call to all_attributes api and print the result.
+    # Make a call to AccountAttribute/AllAttributes api and print the result.
     puts client.all_attributes(account_id)
   end
 end
