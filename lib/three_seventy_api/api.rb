7fcba229761ba8bd6bf54aefb950eccf94b79e62
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 03:15:14 +0530

    callback included in main api file.

--- lib/three_seventy_api/api.rb
+++ lib/three_seventy_api/api.rb
@@ -2,6 +2,7 @@ require "three_seventy_api/api/account"
 require "three_seventy_api/api/account_attribute"
 require "three_seventy_api/api/account_attribute_category"
 require "three_seventy_api/api/account_attribute_definition"
+require "three_seventy_api/api/callback"
 # Api endpoints entry module.
 module ThreeSeventyApi
   module Api
@@ -10,5 +11,6 @@ module ThreeSeventyApi
     include AccountAttribute
     include AccountAttributeCategory
     include AccountAttributeDefinition
+    include Callback
   end
 end
