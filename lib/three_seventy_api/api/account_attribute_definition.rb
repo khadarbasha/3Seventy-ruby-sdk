20387fcfd3f84e4e50a38ae29d9955682b3f3f4b
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 01:32:51 +0530

    delete_atrribute_definition added.

--- lib/three_seventy_api/api/account_attribute_definition.rb
+++ lib/three_seventy_api/api/account_attribute_definition.rb
@@ -10,7 +10,7 @@ module ThreeSeventyApi
         end_point = "/account/#{id}/attributeDef"
         get(end_point)
       end
-      
+
       # Public: Gets the details of a specific attribute definition.
       # 
       # account_id - Integer, Id of the account.
@@ -20,6 +20,17 @@ module ThreeSeventyApi
       def get_attribute_definition(account_id, attribute_id)
         end_point = "/account/#{account_id}/attributeDef/#{attribute_id}"
         get(end_point)
+      end
+
+      # Public: Removes an attribute defintion. Note that when a definition is removed all attributes that are tied to that definition are also removed.
+      # 
+      # account_id - Integer, Id of the account.
+      # attribute_id - Integer, Id of the attribute.
+      #       
+      # Returns none.
+      def delete_atrribute_definition(account_id, attribute_id)
+        end_point = "/account/#{account_id}/attributeDef/#{attribute_id}"
+        delete(end_point)
       end
     end
   end
