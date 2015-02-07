9a66a1f266696acae6e3f5721f24da8b75956a87
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 02:54:57 +0530

    Comments added for client class.

--- lib/three_seventy_api.rb
+++ lib/three_seventy_api.rb
@@ -5,6 +5,17 @@ require 'three_seventy_api/api'
 require 'three_seventy_api/helpers/request'
 require 'pry'
 module ThreeSeventyApi
+  
+  # Public: ThreeSeventyApi client class.
+  # 
+  # url - String, base url of the api.
+  # username - String, username of the user.
+  # password - String, password of the user.
+  # 
+  # => Examples
+  #       - ThreeSeventyApi::Client.new(url, username, password)
+  # 
+  # Returns the api client object.
   class Client
     include Helpers::Request
     attr_reader :username, :password, :url
