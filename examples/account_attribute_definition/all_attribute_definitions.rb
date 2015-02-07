73d218c7d4d8af97f0f1df4ade9dd8874b8b1041
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 01:00:29 +0530

    Example to get the attribute details using AccountAttributeDefinition/AllAttributeDefinitions api added.

---
+++ examples/account_attribute_definition/all_attribute_definitions.rb
@@ -0,0 +1,23 @@
+require 'three_seventy_api'
+require 'yaml'
+# Example to get the attribute details using AccountAttributeDefinition/AllAttributeDefinitions api.
+class AllAttributeDefinitions
+  def run
+    # Load config file
+    config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
+    # Load the account_id from config.
+    account_id = config['account_id']
+    # Load the url from config.
+    url = config['url']
+    # Load the username from config.
+    username = config['username']
+    # Load the password from config.
+    password = config['password']
+    # Initiate the client.
+    client = ThreeSeventyApi::Client.new(url, username, password)
+    # Now make a call to AllAttributeDefinitions api and print the result.
+    puts client.all_attribute_definitions(account_id)
+  end
+end
+example = AllAttributeDefinitions.new
+example.run
