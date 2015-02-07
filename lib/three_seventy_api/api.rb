576cc97c982731d1f5f654d0c311ecf065a00bb9
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 02:55:13 +0530

    comments added for main api module

--- lib/three_seventy_api/api.rb
+++ lib/three_seventy_api/api.rb
@@ -2,8 +2,10 @@ require "three_seventy_api/api/account"
 require "three_seventy_api/api/account_attribute"
 require "three_seventy_api/api/account_attribute_category"
 require "three_seventy_api/api/account_attribute_definition"
+# Api endpoints entry module.
 module ThreeSeventyApi
   module Api
+    # Include every endpoint.
     include Account
     include AccountAttribute
     include AccountAttributeCategory
