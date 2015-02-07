a3b80cf8874b7f18872f3a1f7b80742bd477db73
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-07 16:43:27 +0530

    Example to add a new account using Account/AddAccount api added.

---
+++ examples/account/add_account.rb
@@ -0,0 +1,29 @@
+require 'three_seventy_api'
+# Example to add a new account using Account/AddAccount api.
+class AddAccount
+  def run
+    # Load config file
+    config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
+    # Load the account_id from config.
+    account_id = config['account_id']
+    # Load the url from config.
+    url = config['url']
+    # Load the username from config.
+    username = config['username']
+    # Load the password from config.
+    password = config['password']
+    # Initiate the client.
+    client = ThreeSeventyApi::Client.new(url, username,password)
+    # Add your payload here
+    payload = {
+      "ParentId" => # Set parentId here.,
+      "Name" => # Set Name here.,
+      "Channels" => # Set channels here.,
+      "Attributes"=> # Set attributes here.
+    }.to_json
+    # Make a call to Account/AddAccount api and print the result.
+    puts client.add_account(account_id, payload)
+  end
+end
+example = AddAccount.new
+example.run
