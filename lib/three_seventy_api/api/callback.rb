23a3d301c3c52ff9b9b727a5ac4e254735a6cb0c
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 03:14:03 +0530

    all_callbacks implementation added.

---
+++ lib/three_seventy_api/api/callback.rb
@@ -0,0 +1,18 @@
+module ThreeSeventyApi
+  module Api
+    module Callback
+      def all_callbacks(id)
+        end_point = "/account/{id}/callback"
+        get(end_point)
+      end
+      def add_callback
+      end
+      def edit_callback
+      end
+      def delete_callback
+      end
+      def get_callback
+      end
+    end
+  end
+end
