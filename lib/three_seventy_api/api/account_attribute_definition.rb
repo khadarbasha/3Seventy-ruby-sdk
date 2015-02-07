b5e0f28df4763a54fb4da2d119789b30e2764164
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 02:36:15 +0530

    implementation for add_attribute_definition added.

--- lib/three_seventy_api/api/account_attribute_definition.rb
+++ lib/three_seventy_api/api/account_attribute_definition.rb
@@ -32,6 +32,21 @@ module ThreeSeventyApi
         end_point = "/account/#{account_id}/attributeDef/#{attribute_id}"
         delete(end_point)
       end
+      # Public: Add a new attribute definition.
+      # 
+      # account_id - Integer, account id of the user.
+      # payload - Hash, represents the body of the attribute.
+      #           CategoryId - Integer, CategoryId of the attribute (optional).
+      #           DataType - Integer, DataType of the attribute.
+      #           Name - String, Name of the attribute. 
+      #           Inheritable - Boolean, set if child accounts will get this value from their parents if not directly set.
+      #           Description - String, description of the attribute.
+      #
+      # Returns the newly created attribute.
+      def add_attribute_definition(account_id, payload)
+        end_point = "/account/#{account_id}/attributeDef"
+        post(end_point, payload)
+      end
     end
   end
 end
