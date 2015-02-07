4566a5063d4b1feb24a655c508d10ff4a62a1857
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 01:01:19 +0530

    AccountAttributeDefinition module included in Api module.

--- lib/three_seventy_api/api.rb
+++ lib/three_seventy_api/api.rb
@@ -1,10 +1,12 @@
 require "three_seventy_api/api/account"
 require "three_seventy_api/api/account_attribute"
 require "three_seventy_api/api/account_attribute_category"
+require "three_seventy_api/api/account_attribute_definition"
 module ThreeSeventyApi
   module Api
     include Account
     include AccountAttribute
     include AccountAttributeCategory
+    include AccountAttributeDefinition
   end
 end
