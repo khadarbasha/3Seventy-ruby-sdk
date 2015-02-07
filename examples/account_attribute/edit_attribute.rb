dce23a888bfeb66ddb2701cc3ab5256b6b39ae2a
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-07 19:30:54 +0530

    Example to edit a attribute using AccountAttribute/EditAttribute api added.

--- examples/account_attribute/edit_attribute.rb
+++ examples/account_attribute/edit_attribute.rb
@@ -1,7 +1,7 @@
 require 'three_seventy_api'
 require 'yaml'
 # Example to edit a attribute using AccountAttribute/EditAttribute api.
-class EdirAttribute
+class EditAttribute
   def run
     # Load the config file
     config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
@@ -15,10 +15,18 @@ class EdirAttribute
     password = config['password']
     # Initiate the client.
     client = ThreeSeventyApi::Client.new(url, username, password)
-    name = ##create the attributes new name here.
+    #payload = {
+    #  "t70_contact_quota" => 35
+    #}
+    
+    # Give the key and value details here.
+    payload = {
+      "#fill attribute name here" => "#fill attribute value here"
+    }
+
     # Make a call to EditAttribute api and print the result.
-    client.edit_attribute(account_id, name)
+    puts client.edit_attribute(account_id, payload)
   end
 end
-example = AddAttribute.new
+example = EditAttribute.new
 example.run
