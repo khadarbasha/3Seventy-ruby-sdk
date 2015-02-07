b5b5ab3fa83c3c728496cd169bdbb442aa5d0cb9
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-07 23:32:39 +0530

    account_attribute_category.rb loaded in main api file.

--- lib/three_seventy_api/api.rb
+++ lib/three_seventy_api/api.rb
@@ -1,5 +1,6 @@
 require "three_seventy_api/api/account"
 require "three_seventy_api/api/account_attribute"
+require "three_seventy_api/api/account_attribute_category"
 module ThreeSeventyApi
   module Api
     include Account
