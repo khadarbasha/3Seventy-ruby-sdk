a30ebbcb2efc289312c23cea30bf09bed15cb1b6
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 19:47:55 +0530

    Documentation updated with YARD.

--- lib/three_seventy_api.rb
+++ lib/three_seventy_api.rb
@@ -4,22 +4,21 @@ require 'three_seventy_api/version'
 require 'three_seventy_api/api'
 require 'three_seventy_api/helpers/request'
 require 'pry'
+
 module ThreeSeventyApi
-  
-  # Public: ThreeSeventyApi client class.
-  # 
-  # url - String, base url of the api.
-  # username - String, username of the user.
-  # password - String, password of the user.
-  # 
-  # => Examples
-  #       - ThreeSeventyApi::Client.new(url, username, password)
-  # 
-  # Returns the api client object.
+  # ThreeSeventyApi client class.
   class Client
     include Helpers::Request
     attr_reader :username, :password, :url
     include Api
+
+    # Initializer for client.
+    # 
+    # @param url [String] Base url of the api.
+    # @param username [String] Api Username.
+    # @param password [String] Api password.
+    # 
+    # @return [ThreeSeventyApi::Client]
     def initialize(url, username, password)
       @url = url
       @username = username
