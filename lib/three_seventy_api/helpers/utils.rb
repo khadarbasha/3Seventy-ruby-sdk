d729db0be1ffb331842c599a82889a5aadb6f8a7
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 23:30:36 +0530

    Documentation added.

--- lib/three_seventy_api/helpers/utils.rb
+++ lib/three_seventy_api/helpers/utils.rb
@@ -1,6 +1,11 @@
 module ThreeSeventyApi
   module Helpers
+    # All Utility methods definition will go here.
     module Utils
+      # Convert symbols to string.
+      # @param hash [Hash] Hash with keys as symbols.
+      # 
+      # @return [Hash] Hash with keys as strings.
       def sym_keys_to_str(hash)
         new_hash = {}
         hash.each do |key, value|
