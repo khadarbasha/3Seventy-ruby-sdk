3c14f9755faf189a433d7662baefd290428e23e4
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 19:48:41 +0530

    code formatted.

--- lib/three_seventy_api/api/callback.rb
+++ lib/three_seventy_api/api/callback.rb
@@ -1,17 +1,26 @@
 module ThreeSeventyApi
   module Api
+    # Callback Api end points implementation goes here.
     module Callback
-      def all_callbacks(id)
-        end_point = "/account/{id}/callback"
+      def all_callbacks(account_id)
+        end_point = "/account/#{account_id}/callback"
         get(end_point)
       end
-      def add_callback
+      def add_callback(account_id, payload)
+        end_point = "/account/#{account_id}/callback"
+        post(end_point, payload)
       end
       def edit_callback
+        end_point = "/account/#{account_id}/callback"
+        post(end_point, payload)
       end
-      def delete_callback
+      def delete_callback(account_id, callback_id)
+        end_point = "/account/#{account_id}/callback/#{callback_id}"
+        delete(end_point)
       end
-      def get_callback
+      def get_callback(account_id, callback_id)
+        end_point = "/account/#{account_id}/callback/#{callback_id}"
+        get(end_point)
       end
     end
   end
