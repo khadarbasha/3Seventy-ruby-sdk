d847554e2f7179ae17ea8fcf089f61264a269b82
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 19:48:14 +0530

    Documentation repalced with YARD.

--- lib/three_seventy_api/api/account_attribute_category.rb
+++ lib/three_seventy_api/api/account_attribute_category.rb
@@ -1,18 +1,22 @@
 module ThreeSeventyApi
   module Api
+    # AccountAttributeCategory api end points implementation.
+    # @see  https://api.3seventy.com/docs/v2.0/Default/endpoints#!/accountattributecategory
     module AccountAttributeCategory
-      # Public: Lists all categories for account attributes.
+
+      # Lists all the categories of account attributes.
       # 
-      # Returns the list of all categories of account attributes.
+      # @return [Array<Object>] The list of all categories of account attributes.
       def all_account_attribute_categories
         end_point = "/reference/account-attribute-category"
         get(end_point)
       end
 
-      # Public: Gets the details for a specific account attribute category.
+      # Gets the details for a specific account attribute category.
       # 
-      # id - Integer, Id of the category.
-      #       
+      # @param id [Integer] Id of the category.
+      # @return [Object] the details for a specific account attribute category.
+      #
       # Returns the details for a specific account attribute category.
       def get_account_attribute_category(id)
         end_point = "/reference/account-attribute-category/#{id}"
