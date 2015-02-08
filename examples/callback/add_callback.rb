8ff1883c4ec27078e86fc8b6c98f112b42855638
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-08 23:56:27 +0530

    Example to add new callback added.

---
+++ examples/callback/add_callback.rb
@@ -0,0 +1,29 @@
+require 'three_seventy_api'
+require 'yaml'
+# Example to add new callback.
+class AddCallback
+  def run
+    # Load the config file
+    config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
+    # Load the account_id from config.
+    account_id = config['account_id']
+    # Load the url from config.
+    url = config['url']
+    # Load the username from config.
+    username = config['username']
+    # Load the password from config.
+    password = config['password']
+    # Load ur payload here.
+    payload = {
+      "CallbackType" => ## Fill callback type here.,
+      "URL" => ## Fill url here,
+      "Active" => ## Fill whether to enable this callback or not.
+    }
+    # Initiate the client.
+    client = ThreeSeventyApi::Client.new(url, username, password)
+    # Now make a call to AddCallback api and print the result.
+    puts client.add_callback(account_id, payload)
+  end
+end
+example = AddCallback.new
+example.run
