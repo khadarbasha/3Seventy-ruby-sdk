81002d9f3d2fb11215b77b6bc529cd970c72367c
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-11 01:43:48 +0530

    file added.

---
+++ examples/subscription/all_subscriptions.rb
@@ -0,0 +1,23 @@
+require 'three_seventy_api'
+require 'yaml'
+# Example to list all the subscriptions.
+class AllSubscriptions
+  def run
+    # Load the config file
+    config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
+    # Load the account_id from config.
+    account_id = config['account_id']
+    # Load the url from config.
+    url = config['url']
+    # Load the username from config.
+    username = config['username']
+    # Load the password from config.
+    password = config['password']
+    # Initiate the client.
+    client = ThreeSeventyApi::Client.new(url, username, password)
+    # Now make a call to AllSubscriptions api and print the result.
+    puts client.all_subscriptions(account_id)
+  end
+end
+example = AllSubscriptions.new
+example.run
