f3dbf82f527c1b36573f982b221bbb5c95ab26f4
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-13 07:04:04 +0530

    Api implementation modified by removing account_id.

--- lib/three_seventy_api/api/subscription.rb
+++ lib/three_seventy_api/api/subscription.rb
@@ -3,10 +3,8 @@ module ThreeSeventyApi
     # Subscription Api end point implementation goes here.
     # @see https://api.3seventy.com/docs/v2.0/Default/endpoints#!/subscription
     module Subscription
-
       # Add a new subscription.
       # 
-      # @param account_id [Integer] Id of the account.
       # @param payload [Hash] Body of the subscription.
       # @option payload [String] Name Subscription name.
       # @option payload [String] Label Subscription label.
@@ -14,25 +12,22 @@ module ThreeSeventyApi
       # @option payload [Integer] Frequency (0) Frequency of the subscription.
       # 
       # @return [Object] Newly created subscription object.
-      def add_subscription(account_id, payload)
-        end_point = "/account/#{account_id}/subscription"
-        post(end_point, payload)
+      def add_subscription(payload)
+       # end_point = "/account/#{account_id}/subscription"
+        post(url_("subscription"), payload)
       end
 
       # Retrieve a subscription.
       # 
-      # @param account_id [Integer] Id of the account.
       # @param subscription_id [Integer] Id of the subscription.
       # 
       # @return [Object] Subscription object.
-       def get_subscription(account_id, subscription_id)
-        end_point = "/account/#{account_id}/subscription/#{subscription_id}"
-        get(end_point)
+      def get_subscription(subscription_id)
+        get(url_("subscription", subscription_id))
       end
 
       # Edit a subscription.
       # 
-      # @param account_id [Integer] Id of the account.
       # @param subscription_id [Integer] Id of the subscription.
       # @param payload [Hash] Body of the subscription.
       # @option payload [String] Name Subscription name.
@@ -41,30 +36,25 @@ module ThreeSeventyApi
       # @option payload [Integer] Frequency (0) Frequency of the subscription.
       # 
       # @return [Object] Modified subscription object.
-      def edit_subscription(account_id, subscription_id, payload)
-        end_point = "/account/#{account_id}/subscription/#{subscription_id}"
-        put(end_point, payload)
+      def edit_subscription(subscription_id, payload)
+        put(url_("subscription", subscription_id), payload)
       end
 
       # Delete the subscription.
       # 
-      # @param account_id [Integer] Id of the account.
       # @param subscription_id [Integer] Id of the subscription.
       # 
       # @return none.
       def delete_subscription(account_id, subscription_id)
-        end_point = "/account/#{account_id}/subscription/#{subscription_id}"
-        delete(end_point)
+        delete(url_("subscription", subscription_id))
       end
 
       # Get all the subscriptions assosiated with an account.
       # 
-      # @param account_id [Integer] Id of the account.
       # 
       # @return [Array<Object>] List of all the subscriptions. 
-      def all_subscriptions(account_id)
-        end_point = "/account/#{account_id}/subscription"
-        get(end_point)
+      def all_subscriptions
+        get(url_("subscription"))
       end
     end
   end
