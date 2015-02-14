ab0301e752a816170557480683bf39e218c178e7
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-14 08:18:08 +0530

    un necessary module load removed.

--- lib/three_seventy_api/api.rb
+++ lib/three_seventy_api/api.rb
@@ -1,8 +1,3 @@
-require "three_seventy_api/api/account"
-require "three_seventy_api/api/account_attribute"
-require "three_seventy_api/api/account_attribute_category"
-require "three_seventy_api/api/account_attribute_definition"
-require "three_seventy_api/api/callback"
 require "three_seventy_api/api/content"
 require "three_seventy_api/api/subscription"
 require "three_seventy_api/api/campaign"
@@ -13,11 +8,6 @@ require "three_seventy_api/api/event_push_campaign"
 module ThreeSeventyApi
   module Api
     # Include every endpoint here.
-    # include Account
-    # include AccountAttribute
-    # include AccountAttributeCategory
-    # include AccountAttributeDefinition
-    # include Callback
     include Content
     include Subscription
     include Campaign
