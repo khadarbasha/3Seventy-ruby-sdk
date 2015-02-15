2711024e5b10cd8ed5c7a9c7890b703056ff5dbc
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-15 22:58:41 +0530

    Base models entry added to model entry module.

--- lib/three_seventy_api/model.rb
+++ lib/three_seventy_api/model.rb
@@ -1,3 +1,6 @@
+require "three_seventy_api/model/base"
+require "three_seventy_api/model/base_audited"
+require "three_seventy_api/model/soft_deletable"
 require "three_seventy_api/model/content"
 require "three_seventy_api/model/subscription"
 require "three_seventy_api/model/campaign"
@@ -6,6 +9,8 @@ require "three_seventy_api/model/event_push_campaign"
 module ThreeSeventyApi
   module Model
     include Base
+    include BaseAudited
+    include SoftDeletable
     include Content
     include Subscription
     include Campaign
