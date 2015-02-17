71c67f4d593b257d47aaa69a1ad7e1f83ca864ee
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-17 10:50:49 +0530

    AllContacts example added.

---
+++ examples/contact/all_contacts.rb
@@ -0,0 +1,23 @@
+require 'three_seventy_api'
+require 'yaml'
+# Example to list all the contacts.
+class AllContacts
+  def run
+    # Load the config file
+    config = YAML.load_file(File.expand_path("../../config.yml", __FILE__))
+    # Load the account_id from config.
+    account_id = config['account_id']
+    # Load the url from config.
+    url = config['url']
+    # Load the username from config.
+    username = config['username']
+    # Load the password from config.
+    password = config['password']
+    # Initiate the client.
+    client = ThreeSeventyApi::Client.new(url, username, password, account_id)
+    # Now make a call to AllContacts api and print the result.
+    puts client.all_contacts
+  end
+end
+example = AllContacts.new
+example.run
