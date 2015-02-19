0039a0583ca35f12cf409dcd274ff0ef34199c6f
KhadarBasha S <khadarbasha.s@hotmail.com>
2015-02-19 11:34:27 +0530

    url generator modified.

--- lib/three_seventy_api/helpers/url.rb
+++ lib/three_seventy_api/helpers/url.rb
@@ -1,9 +1,15 @@
 module ThreeSeventyApi
   module Helpers
     module Url
-      def url_ (*args)
-        base_path = "/account/#{account_id}/#{args[0]}"
-        base_path += "/#{args[1]}" if args.length > 1
+      def url_(*args)
+        base_path = "/account/#{account_id}"
+        args.each do |arg|
+          base_path += "/#{arg}"
+        end
+        # binding.pry
+        # base_path = "/account/#{account_id}/#{args[0]}"
+        # base_path += "/#{args[1]}" if args.length > 1
+        # 
         return base_path
       end
     end
